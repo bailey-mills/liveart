fbd814c922a7bac739ad07765af81ef3d331a5eb
Bailey Mills <B_Mills@hotmail.ca>
2021-04-07 04:02:55 -0400

    Move LIVEART stored procedure to main schema file

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -277,3 +277,50 @@ INSERT INTO [dbo].[Tag] (Name, CategoryID) VALUES
 ('Flower bouquet',3),('Pot Plant',3),('Bonsai', 3),
 ('Art Doll',4),('Glass Art Work', 4), ('Jade Art Work', 4), ('Embroidery', 4), ('Cutlery', 4);
 go
+
+-- [LIVEART] Stored Procedures
+use LIVEART;
+-- GET TABLES
+DROP PROCEDURE IF EXISTS GetTables;
+GO
+CREATE PROCEDURE GetTables @db nvarchar(50)
+AS
+	EXEC ('USE ' + @db);
+	SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'
+GO
+
+-- GET ROWS
+DROP PROCEDURE IF EXISTS GetRows;
+GO
+CREATE PROCEDURE GetRows @Requests int, @SampleRows int, @Table nvarchar(50), @Column nvarchar(50)
+AS
+	DECLARE @loops int = @Requests / @SampleRows;
+	DECLARE @extra int = @Requests % @SampleRows;
+
+	if @extra <> 0
+		SET @loops = @loops + 1;
+
+	DECLARE @i int = 0;
+	DECLARE @total int = 0;
+	DECLARE @curr int = 0;
+
+	CREATE TABLE #temptable (Value nvarchar(50));
+
+	WHILE @i < @loops
+	BEGIN
+		-- Determine how many rows to pull (set to max, unless on the last loop and less are needed)
+		SET @curr = @SampleRows;
+		if @extra <> 0 AND @i = @loops - 1
+			SET @curr = @extra;
+
+		-- INSERT into temporary table
+		EXEC('USE [LIVEART]; INSERT INTO #temptable SELECT TOP (' + @curr + ') ' + @Column + ' FROM [dbo].[' + @Table + '] ORDER BY NEWID()');
+
+		-- Increment loop counter
+		SET @i = @i + 1;
+	END;
+
+	SELECT * FROM #temptable;
+
+	DROP TABLE #temptable;
+GO
