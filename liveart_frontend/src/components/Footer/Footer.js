ea37f2a3373af90df19e42792c3ae52015b8be39
Bailey Mills <B_Mills@hotmail.ca>
2021-04-21 17:02:18 -0400

    Revert "Condense footer"
    This reverts commit 462572b71271a044e496c6b9828ede2150799b18.

--- liveart_frontend/src/components/Footer/Footer.js
+++ liveart_frontend/src/components/Footer/Footer.js
@@ -9,31 +9,67 @@ import { useHistory } from "react-router-dom";
 
 
 function Footer(){
-  return(
-      <footer className="bg-dark text-center text-sm-start text-light">
-        <div className="text-center text-light p-3" >
-          <h5 className="text-uppercase">Programmers - Team Sherba</h5>
-  
-          <ul className="list-unstyled mb-0">
-            <li>
-              <a href="#!" className="text-light">Eric Lin</a>
-            </li>
-            <li>
-              <a href="#!" className="text-light">Shuang Liang</a>
-            </li>
-            <li>
-              <a href="#!" className="text-light">Bailey Mills</a>
-            </li>
-          </ul>
-        </div>
-        
-        <div className="text-center text-light p-3">
-          © 2020 Copyright:
-          <a> Team Sherba</a>
-        </div>
 
-      </footer>
-  )
+
+        return(       
+            
+
+            <footer className="bg-dark text-center text-sm-start text-light">
+
+              <div className="container p-4">
+
+                <div className="row">
+
+                  <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+                    <h5 className="text-uppercase text-light">LIVE.ART</h5>
+            
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                      molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                      voluptatem veniam, est atque cumque eum delectus sint!
+                    </p>
+                  </div>
+
+                  <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 className="text-uppercase">Programmers - Team Sherba</h5>
+            
+                    <ul className="list-unstyled mb-0">
+                      <li>
+                        <a href="#!" className="text-light">Eric Lin</a>
+                      </li>
+                      <li>
+                        <a href="#!" className="text-light">Shuang Liang</a>
+                      </li>
+                      <li>
+                        <a href="#!" className="text-light">Bailey Mills</a>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 className="text-uppercase mb-0">Source Code</h5>
+            
+                    <ul className="list-unstyled">
+                      <li>
+                        <a href="#!" className="text-dark">Github Link</a>
+                      </li>
+                    </ul>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className="text-center text-light p-3" >
+                © 2020 Copyright:
+                <a className="text-light" href="https://mdbootstrap.com/">Team Sherba</a>
+              </div>
+
+            </footer>
+
+            
+        )
+    
 }
 
 export default Footer;
